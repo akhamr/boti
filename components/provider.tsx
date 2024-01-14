d459f07494ad47c6d102e550877182a21ee26619
Khamid Arrazaq <saikonomia@gmail.com>
2024-01-14 23:17:00 +0700

    thanks

--- components/provider.tsx
+++ components/provider.tsx
@@ -3,8 +3,18 @@
 import { SidebarProvider } from "@/lib/hooks/use-sidebar";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { useEffect } from "react";
+import { useOverlayScrollbars } from "overlayscrollbars-react";
+import "overlayscrollbars/overlayscrollbars.css";
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
+    const [init, _] = useOverlayScrollbars({
+        defer: true,
+    });
+
+    useEffect(() => {
+        init(document.body);
+    }, [init]);
     return (
         <NextThemesProvider {...props}>
             <SidebarProvider>{children}</SidebarProvider>
