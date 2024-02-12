48039865b262222399804c30bbac201e529d4f71
Khamid Arrazaq <saikonomia@gmail.com>
2024-02-12 13:39:11 +0700

    update: sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
                 },
             },
             borderRadius: {
-                md: "var(--radius)",
+                lg: "var(--radius)",
                 md: "calc(var(--radius) - 2px)",
                 sm: "calc(var(--radius) - 4px)",
             },
